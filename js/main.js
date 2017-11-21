e02fb6ed10e86a2fe0468a50e0af10dafaafbddf
999999sy <33650909+999999sy@users.noreply.github.com>
2017-11-21 18:13:42 +0900

    아직도 다 안끝남 ㅋ
    ㅋ

--- js/main.js
+++ js/main.js
@@ -29,33 +29,61 @@ function section_1(){
 
 
 function section_4(){
-  $('.christmas_left img').css({"opacity":'0',"left":'-100px'}).delay(1000).animate({"opacity":'1',"left":'21%'},1500);
+  $(window).scroll(function(){
+    var pos = $('.section_4').offset().top;
+    var top = $(window).scrollTop();
+    if(pos>=top+600){
+      $('.christmas_left img').css({"opacity":'0',"left":'-100px'}).delay(1000).animate({"opacity":'1',"left":'21%'},1500);
 
 
-  $(".christmas_right1 a img").css({"opacity":'0',"right":"-100px"}).delay(1000).animate({"opacity":'1',"right":'39%'},1500);
+      $(".christmas_right1 a img").css({"opacity":'0',"right":"-100px"}).delay(1000).animate({"opacity":'1',"right":'39%'},1500);
 
-    $(".christmas_right2 a img").css({"opacity":'0',"right":"-100px"}).delay(1000).animate({"opacity":'1',"right":'39%'},1500);
+        $(".christmas_right2 a img").css({"opacity":'0',"right":"-100px"}).delay(1000).animate({"opacity":'1',"right":'39%'},1500);
 
-      $(".christmas_right3 a span").css({"opacity":'0',"right":"-100px","transition":"0s"}).delay(1000).animate({"opacity":'1',"right":'39%',"transition":'0.5s'},1500);
+          $(".christmas_right3 a span").css({"opacity":'0',"right":"-100px","transition":"0s"}).delay(1000).animate({"opacity":'1',"right":'39%',"transition":'0.5s'},1500);
+    }
+  })
+
 
 }
 
 
 function section_5(){
-  $('.reserved_products img').css({"opacity":'0'}).delay(1500).animate({"opacity":'1'},1500);
+  $(window).scroll(function(){
+    var pos = $('.section_5').offset().top;
+    var top = $(window).scrollTop();
+    if(pos>=top+1200){
+        $('.reserved_products img').css({"opacity":'0'}).delay(2000).animate({"opacity":'1'},1500);
+    }
+  })
+
 
 }
 
 function section_6(){
-  $('.pick_title').css({"opacity":'0',"left":'-100px'}).delay(1000).animate({"opacity":'1',"left":'23%'},1500);
+  $(window).scroll(function(){
+    var pos = $('.section_6').offset().top;
+    var top = $(window).scrollTop();
+    if(pos>=top+1500){
+  $('.pick_title').css({"opacity":'0',"left":'-100px'}).delay(3000).animate({"opacity":'1',"left":'23%'},1500);
+    }
+  })
+
 }
 
 
 function section_8(){
+  $(window).scroll(function(){
+    var pos = $('.section_8').offset().top;
+    var top = $(window).scrollTop();
+    if(pos>=top+1600){
 
-  $('.store_find_left img').css({"opacity":'0'}).delay(1500).animate({"opacity":'1'},'slow');
+        $('.store_find_left img').css({"opacity":'0'}).delay(3000).animate({"opacity":'1'},2000);
 
-    $('.store_find_right').css({"opacity":'0',"right":'-21%'}).delay(1500).animate({"opacity":'1',"right":'21%'},1500);
+          $('.store_find_right').css({"opacity":'0',"right":'-21%'}).delay(3000).animate({"opacity":'1',"right":'21%'},2000);
+    }
+  })
+
 }
 
 
