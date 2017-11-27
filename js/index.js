8ba79cc78059d652dfcb8d4a79ec8f3254d4e253
meronaaa <33471288+meronaaa@users.noreply.github.com>
2017-11-27 12:25:18 +0900

    Merge branch 'develope' of https://github.com/object402/starbucks into develope

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@ $(function(){
   $('.wrap_subNav').hide();
 
   $('.wrap_mainNav>ul>li').on('hover',function(){
+<<<<<<< HEAD
     $('.wrap_subNav').stop().slideToggle();
+=======
+    $(this).children('.wrap_subNav').stop(true, true).slideToggle();
+>>>>>>> 7cc421e88598f21252e145a5078216cc69d4422f
   });
 
   var topNavW = $('.topNav ul').width();
-  $('.search').css('margin-left',topNavW);
-  console.log(topNavW);
+  $('.search').css('margin-left',topNavW-1);
 });
