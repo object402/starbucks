b3e19c116baf4ddcd04bb1cdb4b890b5629d5b80
imjiApril <32213780+imjiApril@users.noreply.github.com>
2018-02-01 17:45:44 +0900

    네비 요동 수정

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 $(function(){
   $('.wrap_subNav').hide();
 
-  $('.wrap_mainNav>ul>li').stop().on('hover',function(){
-    $(this).children('.wrap_subNav').stop(true, true).slideToggle();
-  });
+  //네비
+  $('.wrap_mainNav>ul>li').on('mouseover',function(){
+     $('.wrap_subNav').stop().slideUp();
+     $(this).children('.wrap_subNav').stop().slideDown();
+ })
+ $('.wrap_mainNav>ul>li').on('mouseleave',function(){
+    $('.wrap_subNav').stop().slideUp();
+})
+
+
+  //서치 아이콘 위치
   var topNavW = $('.topNav ul').width();
   $('.search').css('margin-left',topNavW-1);
 });
