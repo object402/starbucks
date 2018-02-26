4b5055fc466ae84b6c75584747f1cf05108a4ee1
999999sy <33650909+999999sy@users.noreply.github.com>
2018-02-26 16:38:24 +0900

    nav 수정

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 $(function(){
   $('.wrap_subNav').hide();
 
-  $('.wrap_mainNav>ul>li').on('hover',function(){
-    $(this).children('.wrap_subNav').stop(true, true).slideToggle();
-  });
+  //네비
+  //걍 이걸로 하자.
+  $('.wrap_mainNav>ul>li').on('mouseenter',function(){
+     //$('.wrap_subNav').stop().slideUp();
+     $(this).children('.wrap_subNav').stop().slideDown();
+ })
+ $('.wrap_mainNav>ul>li').on('mouseleave',function(){
+    $('.wrap_subNav').stop().slideUp();
+})
   var topNavW = $('.topNav ul').width();
   $('.search').css('margin-left',topNavW-1);
 });
