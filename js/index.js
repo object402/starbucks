ccc781113e1f509b885afbf09aa350df74a4ead2
imjiApril <32213780+imjiApril@users.noreply.github.com>
2018-03-15 02:20:54 +0900

    .

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ $(function(){
   $('.wrap_subNav').hide();
 
   //네비
-  //걍 이걸로 하자.
   $('.wrap_mainNav>ul>li').on('mouseenter',function(){
      //$('.wrap_subNav').stop().slideUp();
      $(this).children('.wrap_subNav').stop().slideDown();
@@ -10,6 +9,7 @@ $(function(){
  $('.wrap_mainNav>ul>li').on('mouseleave',function(){
     $('.wrap_subNav').stop().slideUp();
 })
+
   var topNavW = $('.topNav ul').width();
   $('.search').css('margin-left',topNavW-1);
 });
